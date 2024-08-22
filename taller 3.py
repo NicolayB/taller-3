8a2632c7a15dc9a2fe961fdec146f47b9cd23394
NicolayB <ne.barrera@uniandes.edu.co>
2024-08-22 08:42:40 -0500

    Datos y graficos

--- taller 3.py
+++ taller 3.py
@@ -11,3 +11,19 @@ Y = df[df.columns[-1]]
 
 # Estadísticas descriptivas de los precios de venta
 print(Y.describe())
+
+# Gráficos de las variables
+print(sns.pairplot(df.iloc[:,1:]))
+
+correl = df.iloc[:,1:].corr()
+print(sns.heatmap(correl, cmap="Blues", annot=True))
+
+# Correlación de las variables y los preciosa
+correlacion = pd.DataFrame(X.corrwith(Y))
+print(sns.heatmap(correlacion, cmap="Blues", annot=True))
+
+# Diagramas de regresión para cada variable
+colum = X.columns
+print(sns.pairplot(df, x_vars=colum[0:2], y_vars="Y house price of unit area", height=7, kind="reg"))
+print(sns.pairplot(df, x_vars=colum[2:4], y_vars="Y house price of unit area", height=7, kind="reg"))
+print(sns.pairplot(df, x_vars=colum[4:], y_vars="Y house price of unit area", height=7, kind="reg"))
